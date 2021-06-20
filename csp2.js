7416cb104a74cf6bcf6cf713c95897f9abacf2c0
AusCryptor82 <shaun.d.dole@gmail.com>
2021-06-20 11:10:20 +1000

    Update csp2.js

--- csp2.js
+++ csp2.js
@@ -1,1 +1,8 @@
-<script>alert(document.cookie);</script>
+    let url = 'https://kewl-site.net/wectf/test.php';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+
